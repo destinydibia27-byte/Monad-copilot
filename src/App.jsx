7b9c2098843d42603ce383200521ebf5966c5a33
destinydibia27-byte <destinydibia27@gmail.com>
2026-06-21 09:54:39 +0100

    feat: remove v1 MVP badge

--- src/App.jsx
+++ src/App.jsx
@@ -192,15 +192,6 @@ useEffect(() => {
               </div>
             </div>
 
-            <span style={{
-              fontFamily: "'IBM Plex Mono',monospace", fontSize: 9, fontWeight: 500,
-              letterSpacing: "0.1em", textTransform: "uppercase",
-              color: T.purple, border: `1px solid ${T.purple}40`,
-              padding: "3px 7px", borderRadius: 4, flexShrink: 0,
-            }}>v1 MVP</span>
-          </div>
-
-          <div style={{ display: "flex", alignItems: "center", gap: 10, width: isMobile ? "100%" : "auto" }}>
             {/* User avatar + sign out */}
             {user && (
               <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
